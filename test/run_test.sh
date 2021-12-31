1b47267a98e8ebc94f5f90bc5b00ecaa0bf7230a
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2021-12-31 19:24:25 +0900

    Update packages.

--- test/run_test.sh
+++ test/run_test.sh
@@ -6,7 +6,7 @@ echo "> python version"
 python --version
 
 echo "> python shared library"
-ls -l ~/.pyenv/versions/3.9.6/lib/libpython3.so
+ls -l ~/.pyenv/versions/3.9.9/lib/libpython3.so
 
 echo "> pipenv version"
 pipenv --version
