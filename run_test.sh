54903282e1ab7adac0202c511d458a7da6cdf959
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2022-07-07 08:57:46 +0900

    Change the way to install dependencies.

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
+set -e
+
 # run test of this project
 
 compose_file=$1/docker-compose.test.yml
-docker-compose -f $compose_file up --build
-docker-compose -f $compose_file down
+docker compose -f $compose_file up --build
+docker compose -f $compose_file down
