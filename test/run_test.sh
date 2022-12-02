40f227d5d1eaae9ae7befa3e7e643203807d817e
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2022-12-02 22:25:25 +0900

    Update packages.

--- test/run_test.sh
+++ test/run_test.sh
@@ -6,7 +6,7 @@ echo "> python version"
 python --version
 
 echo "> python shared library"
-ls -l ~/.pyenv/versions/3.9.14/lib/libpython3.so
+ls -l ~/.pyenv/versions/3.10.8/lib/libpython3.so
 
 echo "> pipenv version"
 pipenv --version
@@ -15,4 +15,5 @@ echo "> test pipenv"
 rm -rf project
 mkdir project
 cd project
+pipenv --python 3.10
 pipenv install sphinx pylint pytest
