01b29a2a29380dac447e312b17180dbad7756ba6
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2022-12-08 23:45:19 +0900

    Make all installed Python versions usable.

--- test/run_test.sh
+++ test/run_test.sh
@@ -5,6 +5,12 @@ cd $(dirname $0)
 echo "> python version"
 python --version
 
+echo "> python3.x versions"
+python3.8 --version
+python3.9 --version
+python3.10 --version
+python3.11 --version
+
 echo "> python shared library"
 ls -l ~/.pyenv/versions/3.10.9/lib/libpython3.so
 
